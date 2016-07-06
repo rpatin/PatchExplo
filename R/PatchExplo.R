7a032a9511f26434e90104a47463bd7f21ffb56f
rpatin <remi.patin@free.fr>
2016-07-06 14:09:16 +0200

    First Commit

---
+++ R/PatchExplo.R
@@ -0,0 +1,13 @@
+#' PatchExplo: Describing patch exploration
+#'
+#' PatchExplo provides usefull tools to describe patterns of patch exploration.
+#' For now it works only on patch organized around central points i.e. a waterhole
+#' and describe exploration on successive day for each distance to water.
+#'
+#' @docType package
+#' @name PatchExplo
+#' @import assertthat
+#' @importFrom utils head tail
+#' @importFrom Rcpp cppFunction Rcpp.plugin.maker
+#' @importFrom stats setNames update
+NULL
